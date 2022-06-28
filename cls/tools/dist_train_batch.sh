57cb98b2cc786260126ffafdfbfb2495cfa10f83
yangxingyi <yangxingyi@sensetime.com>
2022-06-29 01:44:03 +0800

    1. Create a conda environment and activate it. Install PyTorch following official instructions,

--- cls/tools/dist_train_batch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-CUDA_VISIBLE_DEVICES=2 bash tools/dist_train.sh configs/IFC/wideresnet28-2_mobilenet_b128x1_cifar10_logit_deep_l2_paralell_10task_1e-3_taskloss.py 1
-CUDA_VISIBLE_DEVICES=2 bash tools/dist_train.sh configs/IFC/resnet18_mobilenet_b128x1_cifar10_logit_deep_l2_paralell_singletask_1e-3_taskloss.py 1
-CUDA_VISIBLE_DEVICES=2 bash tools/dist_train.sh configs/IFC/resnet18_mobilenet_b128x1_cifar10_logit_deep_l2_paralell_10task_1e-3_taskloss.py 1
