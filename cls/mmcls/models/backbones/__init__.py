56df6bc37f2ac4700b19d758aa03d7d29cb3534c
yangxingyi <yangxingyi@sensetime.com>
2022-06-28 22:30:30 +0800

    from .wideresnet import WideResNet_CIFAR

--- cls/mmcls/models/backbones/__init__.py
+++ cls/mmcls/models/backbones/__init__.py
@@ -7,11 +7,3 @@ from .shufflenet_v2 import ShuffleNetV2
 from .tsn import TSN_backbone
 from .wideresnet import WideResNet_CIFAR
 from .disentangle import SimpleConv64, SimpleGaussianConv64
-
-__all__ = [
-    'LeNet5', 'AlexNet', 'VGG', 'RegNet', 'ResNet', 'ResNeXt', 'ResNetV1d',
-    'ResNeSt', 'ResNet_CIFAR', 'SEResNet', 'SEResNeXt', 'ShuffleNetV1',
-    'ShuffleNetV2', 'MobileNetV2', 'MobileNetV3', 'VisionTransformer',
-    'SwinTransformer', 'TNT', 'TIMMBackbone', 'MobileNetV2_CIFAR', 'TSN',
-    'WideResNet_CIFAR', 'TSN_backbone', 'SimpleConv64', 'SimpleGaussianConv64'
-]
