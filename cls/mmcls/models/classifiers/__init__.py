5face5751e6f71af0063fdcdb4fa910091f355fe
yangxingyi <yangxingyi@sensetime.com>
2022-06-28 22:34:42 +0800

    KFImageClassifier

--- cls/mmcls/models/classifiers/__init__.py
+++ cls/mmcls/models/classifiers/__init__.py
@@ -1,20 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import BaseClassifier
-from .image import (ImageBranchClassifier, ImageClassifier,
-                    ImagePseudoMultiTaskClassifier, MultiTaskImageClassifier)
+from .image import ImageClassifier
 from .kd import KDImageClassifier
 from .kf import KFImageClassifier
 
 
-__all__ = ['BaseClassifier', 'ImageClassifier',
-           'KDImageClassifier', 'KFactorImageClassifier', 'KFactorImageClassifier2',
-           'KFactorImageClassifier_Additive', 'KFactorImageClassifier_Ortho_Additive',
-           'KFactorImageClassifier_no_Additive', 'KDDeepClassifier',
-           'KnowledgeRelationClassifier', 'KFMulHeadImageClassifier',
-           'ImagePseudoMultiTaskClassifier', 'MultiTaskImageClassifier',
-           'KFPMTImageClassifier', 'KFPMTSeriesImageClassifier', 'KFPMTParralImageClassifier',
-           'KFactorParallelImageClassifier', 'KFactorParallelAttnImageClassifier', 'KFactorParallelAddImageClassifier',
-           'KFactorBranchImageClassifier', 'KFactorSeriesImageClassifier', 'KFactorBranchImageClassifier2',
-           'KFactorBranchImageClassifier_cat', 'KFactorBranchImageClassifier_ConvCat',
-           'KFTaskParallelImageClassifier', 'ImageBranchClassifier', 'ImageVibClassifier',
-           'KFTaskParallelImageClassifierv2', 'KDDeepClassifier_InfoMax','KDImageClassifier_Select']
+
