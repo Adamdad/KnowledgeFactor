93f270558633a3bbdbd51f24bbcd25c1fe3d6f1c
xyang <xyang@bytedance.com>
2022-07-18 02:08:54 +0800

    dict(type='BatchCutMix', alpha=1.0, num_classes=1000, prob=0.5)

--- cls/configs/imagenet-kf/resnet18_mbnv2_b32x8_imagenet_softtar_kf.py
+++ cls/configs/imagenet-kf/resnet18_mbnv2_b32x8_imagenet_softtar_kf.py
@@ -26,8 +26,8 @@ model = dict(
                  temperature=2.0),
     train_cfg=dict(
         augments=[
-            dict(type='BatchMixup', alpha=0.1,
-                 num_classes=1000, prob=0.5)
+            dict(type='BatchMixup', alpha=0.1, num_classes=1000, prob=0.5),
+            dict(type='BatchCutMix', alpha=1.0, num_classes=1000, prob=0.5)
         ],
         lambda_kd=1.0,
         lambda_feat=1.0,
