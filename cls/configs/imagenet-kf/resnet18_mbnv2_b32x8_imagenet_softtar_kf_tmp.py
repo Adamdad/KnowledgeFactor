25498869f68becce38e31d608c97dd665cbd5026
xyang <xyang@bytedance.com>
2022-07-26 13:56:23 +0800

    lambda_kd=0.1,

--- cls/configs/imagenet-kf/resnet18_mbnv2_b32x8_imagenet_softtar_kf_tmp.py
+++ cls/configs/imagenet-kf/resnet18_mbnv2_b32x8_imagenet_softtar_kf_tmp.py
@@ -25,7 +25,10 @@ model = dict(
     kd_loss=dict(type='SoftTarget',
                  temperature=2.0),
     train_cfg=dict(
-        lambda_kd=1.0,
+        augments=[
+            dict(type='BatchMixup', alpha=0.1, num_classes=1000, prob=0.5)
+        ],
+        lambda_kd=0.1,
         lambda_feat=1.0,
         alpha=1.0,
         beta=1e-3,
